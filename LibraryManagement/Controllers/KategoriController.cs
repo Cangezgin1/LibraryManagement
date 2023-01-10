98714e0c29daa09501725e4b2a72b1706934d0b1
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2023-01-10 17:24:53 +0300

    Kategori ve Yazarlar için CRUD İşlemleri ve Alert Kullanımı.

--- LibraryManagement/Controllers/KategoriController.cs
+++ LibraryManagement/Controllers/KategoriController.cs
@@ -3,16 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LibraryManagement.Models.Entity;
 
 namespace LibraryManagement.Controllers
 {
     public class KategoriController : Controller
     {
 
+        DbKütüphaneEntities db = new DbKütüphaneEntities();
+
+
+
+
         public ActionResult Index()
+        {
+            var values = db.TblKategori.ToList();
+            return View(values);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult KategoriEkle(TblKategori tblKategori)
+        {
+            db.TblKategori.Add(tblKategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
         
+
+        
+        
+        public ActionResult KategoriSil(int id)
+        {
+            var values = db.TblKategori.Find(id);
+            db.TblKategori.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktg = db.TblKategori.Find(id);
+            return View("KategoriGetir", ktg);
+        }
+        public ActionResult KategoriGüncelle(TblKategori tblKategori)
+        {
+            var values = db.TblKategori.Find(tblKategori.Id);
+            values.Ad = tblKategori.Ad;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ LibraryManagement/Controllers/YazarController.cs
@@ -0,0 +1,66 @@
+﻿using LibraryManagement.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class YazarController : Controller
+    {
+
+        DbKütüphaneEntities db = new DbKütüphaneEntities();
+
+
+        public ActionResult Index()
+        {
+            var values = db.TblYazar.ToList();
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public ActionResult YazarEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YazarEkle(TblYazar tblYazar)
+        {
+            db.TblYazar.Add(tblYazar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        public ActionResult YazarSil(int id)
+        {
+            var yazar = db.TblYazar.Find(id);
+            db.TblYazar.Remove(yazar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        public ActionResult YazarGetir(int id)
+        {
+            var values = db.TblYazar.Find(id);
+            return View("YazarGetir", values);
+        }
+        public ActionResult YazarGüncelle(TblYazar tblYazar)
+        {
+            var values = db.TblYazar.Find(tblYazar.Id);
+            values.Ad = tblYazar.Ad;
+            values.Soyad = tblYazar.Soyad;
+            values.Detay = tblYazar.Detay;
+            db.SaveChanges();
+            return RedirectToAction("Index","Yazar");
+        }
+
+    }
+}
