1441a738d08c7aafe759e64090dd1577cf1c838b
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2023-01-11 14:43:02 +0300

    Kitap Listesinde Arama yapma ve KitapGüncelleme

--- LibraryManagement/Controllers/KitapController.cs
+++ LibraryManagement/Controllers/KitapController.cs
@@ -13,13 +13,22 @@ namespace LibraryManagement.Controllers
         DbKütüphaneEntities db = new DbKütüphaneEntities();
 
 
+        #region Kitap Listesi
 
-        public ActionResult Index()
+        public ActionResult Index(string p)
         {
-            var kitaplar = db.TblKitap.ToList();
-            return View(kitaplar);
+            var kitaplar = from k in db.TblKitap select k;
+            if (!string.IsNullOrEmpty(p))
+            {
+                kitaplar = kitaplar.Where(x => x.Ad.Contains(p));
+            }
+            //var kitaplar = db.TblKitap.ToList();
+            return View(kitaplar.ToList());
         }
-        
+
+        #endregion
+
+        #region Kitap Ekle
 
         [HttpGet]
         public ActionResult KitapEkle()
@@ -55,7 +64,9 @@ namespace LibraryManagement.Controllers
             return RedirectToAction("Index","Kitap");
         }
 
+        #endregion
 
+        #region Kitap Sil
 
         public ActionResult KitapSil(int id)
         {
@@ -64,6 +75,46 @@ namespace LibraryManagement.Controllers
             db.SaveChanges();
             return RedirectToAction("Index", "Kitap");
         }
+
+        #endregion
+
+        #region Kitap Güncelleme
+
+        public ActionResult KitapGetir(int id)
+        {
+            var values = db.TblKitap.Find(id);
+            List<SelectListItem> deger1 = (from i in db.TblKategori.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.Ad,
+                                               Value = i.Id.ToString()
+                                           }).ToList();
+            ViewBag.deger1 = deger1;
+            List<SelectListItem> deger2 = (from i in db.TblYazar.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.Ad + ' ' + i.Soyad,
+                                               Value = i.Id.ToString()
+                                           }).ToList();
+            ViewBag.deger2 = deger2;
+            return View("KitapGetir", values);
+        }
+        public ActionResult KitapGüncelle(TblKitap tblKitap)
+        {
+            var kitap = db.TblKitap.Find(tblKitap.Id);
+            kitap.Ad = tblKitap.Ad;
+            kitap.BasımYıl = tblKitap.BasımYıl;
+            kitap.SayfaSayısı = tblKitap.SayfaSayısı;
+            kitap.YayınEvi = tblKitap.YayınEvi;
+            var ktg = db.TblKategori.Where(x => x.Id == tblKitap.TblKategori.Id).FirstOrDefault();
+            var yzr = db.TblYazar.Where(x => x.Id == tblKitap.TblYazar.Id).FirstOrDefault();
+            kitap.Kategori = ktg.Id;
+            kitap.Yazar = yzr.Id;
+            db.SaveChanges();
+            return RedirectToAction("Index","Kitap");
+        }
+
+        #endregion
 
     }
 }
